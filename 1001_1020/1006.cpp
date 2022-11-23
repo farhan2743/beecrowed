21ea4ada1e8ad969d03801a7b9e45ac5164ed18b
farhan2743 <hfarhan2743@gmail.com>
2022-11-23 13:00:29 +0600

    hh

--- 1001_1020/1006.cpp
+++ 1001_1020/1006.cpp
@@ -14,3 +14,12 @@ Output
 Print the message "MEDIA"(average in Portuguese) 
 and the student's average according to the following example, with a blank space before and after the equal signal.*/
 
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{   
+    double a,b,c,media;
+    scanf("%lf%lf%lf",&a,&b,&c);
+    media = (a/10*2)+(b/10*3)+(c/10*5);
+    printf("MEDIA = %.1lf\n",media);
+    return 0;
+}
---
+++ 1001_1020/1009.cpp
@@ -0,0 +1,34 @@
+/*Make a program that reads a seller's name, 
+his/her fixed salary and the sale's total made 
+by himself/herself in the month (in money). 
+Considering that this seller receives 15% over 
+all products sold, write the final salary (total)
+ of this seller at the end of the month , with 
+ two decimal places.
+
+- Don’t forget to print the line's end after the
+ result, otherwise you will receive “Presentation 
+ Error”.
+
+- Don’t forget the blank spaces.
+
+Input
+The input file contains a text (employee's first name),
+ and two double precision values, that are the seller's
+  salary and the total value sold by him/her.
+
+Output
+Print the seller's total salary, according to the given 
+example.*/
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    char name;
+    double fixed_salary, product_sold,total;
+    scanf("%s",&name);
+    scanf("%lf%lf",&fixed_salary,&product_sold);
+    total = fixed_salary + (product_sold*.15);
+    printf("TOTAL = R$ %.2lf\n",total);
+
+    return 0;
+}
